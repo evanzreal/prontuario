c2e162ee5f9975d6820f3afe9c75fde2a9c628e5
Seu Nome <seu.email@exemplo.com>
2025-04-02 23:44:40 -0300

    Corrige configuração para deploy na Vercel

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  output: 'standalone',
   env: {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
